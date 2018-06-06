ba8b00acd801866891a8708bb66b43ce202a57b3
abw012 <dennis.eickholt@haw-hamburg.de>
2018-06-06 14:11:53 +0200

    Init

--- src/main/java/quicksort/pivotStrategie/PivotStrategieFix.java
+++ src/main/java/quicksort/pivotStrategie/PivotStrategieFix.java
@@ -1,7 +1,13 @@
 package quicksort.pivotStrategie;
 
+import quicksort.SchluesselWertPaar;
+
 /**
  * @author ${user} on ${date}
  */
-public class PivotStrategieFix {
+public class PivotStrategieFix implements PivotStrategie {
+    @Override
+    public <T extends Comparable<T>, U> int getIndex(SchluesselWertPaar<T, U>[] a, int iLinks, int iRechts) {
+        return 0;
+    }
 }
